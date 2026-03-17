0e1f31ccb5e051a836ee70091e28f32ed3da8d26
Artem <artemij.mosckowsky.01062008@gmail.com>
2026-03-17 12:14:32 +0200

    Show response ids

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -4,6 +4,8 @@ async function getUser() {
   try {
     const response = await axios.get("https://fakestoreapi.com/products");
     console.log(response.data);
+    console.log("Response ids:")
+    console.log(response.data.map(e => e.id));
   } catch (error) {
     console.error(`Error fetching user: ${error}`);
   }
